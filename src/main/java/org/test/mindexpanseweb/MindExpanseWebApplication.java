6dbd43323bb859ab037a59f845045c8840399764
BugingoBlaise <blaisemugisha1@gmail.com>
2024-12-06 13:29:22 +0200

    now okay

--- src/main/java/org/test/mindexpanseweb/MindExpanseWebApplication.java
+++ src/main/java/org/test/mindexpanseweb/MindExpanseWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MindExpanseWebApplication {
-
+//hello
     public static void main(String[] args) {
         SpringApplication.run(MindExpanseWebApplication.class, args);
     }
