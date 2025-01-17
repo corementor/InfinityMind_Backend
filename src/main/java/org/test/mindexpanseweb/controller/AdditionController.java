4faf47977fb8b0f8d6897e4afa76624eaa34ece8
BugingoBlaise <blaisemugisha1@gmail.com>
2025-01-17 10:40:46 +0200

    Auth added

--- src/main/java/org/test/mindexpanseweb/controller/AdditionController.java
+++ src/main/java/org/test/mindexpanseweb/controller/AdditionController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@CrossOrigin(origins = "https://mind-expanse.vercel.app/")
-//@CrossOrigin(origins = "http://localhost:5173/")
+//@CrossOrigin(origins = "https://mind-expanse.vercel.app/")
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/api/math/addition")
 
 @Slf4j
